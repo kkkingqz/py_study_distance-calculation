fba7decdea65bf3d99dce53c349a812967378707
Bodya <kkingqz@gmail.com>
2022-08-24 17:25:19 +0300

    check input function

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import math as m
 
-RADIUS = 6371.01
-is_correct = False
-
-lat1 = m.radians(50.45)
-lon1 = m.radians(30.523)
-
-lat2 = m.radians(51.5072)
-lon2 = m.radians(-0.1275)
-while not is_correct:
-    lat1_e = ''
-    lat1_e = input("Enter lat for 1'st city:    (default: Kyiv 50.45)\n")
-    if lat1_e:
-        try:
-            lat1 = float(lat1_e)
+def check_input(default_value, text):
+    is_correct = False
+    input_value = ''
+    while not is_correct:
+        input_value = input(text)
+        if input_value:
+            try:
+                output_value = float(input_value)
+                is_correct = True
+            except ValueError:
+                print("not a correct value")
+                is_correct = False
+        else:
             is_correct = True
-        except ValueError:
-            print("not a correct value")
-    else:
-        is_correct = True
+            output_value = default_value
+    return output_value
+
+#default values
+RADIUS = 6371.01
+lat1 = 50.45
+lon1 = 30.523
+lat2 = 51.5072
+lon2 = -0.1275
+#---
+
+lat1 = m.radians(check_input(lat1, "Enter lat for the 1'st city:    (default: Kyiv 50.45)\n"))
+lon1 = m.radians(check_input(lon1, "Enter lon for the 1'st city:    (default: Kyiv 30.523)\n"))
+lat2 = m.radians(check_input(lat2, "Enter lat for the 2'nd city:    (default: London 51.5072)\n"))
+lon2 = m.radians(check_input(lon2, "Enter lon for the 2'nd city:    (default: London -0.1275)\n"))
+
+
 
 distance = RADIUS*(m.acos(m.sin(lat1)*m.sin(lat2)+m.cos(lat1)*m.cos(lat2)*m.cos(lon1-lon2)))
 
